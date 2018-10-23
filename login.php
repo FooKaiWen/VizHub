d8d56b7d3fb459c817a36289305e793cf1f5a960
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2018-10-23 19:07:35 +0800

    Loading screen added.

--- login.php
+++ login.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>VizHub</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="design.css">
+  <link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+  
+<div class="lds-hourglass">Loading...</div>
+
 <?php
 
 require_once  'Facebook/autoload.php';
@@ -21,3 +37,6 @@ $loginUrl = $helper->getLoginUrl('https://localhost/VizHub/fb-callback.php', $pe
   }
 
 ?>
+
+</body>
+</html>
