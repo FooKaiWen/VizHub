493b3db4b16449e612a22f0c5c93507a4cee0149
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2018-11-07 15:34:44 +0800

    Bug fixed on alert message.

--- fb-callback.php
+++ fb-callback.php
@@ -6,7 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="design.css">
   <link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css">
-  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="chart.js"></script>
 </head>
 <body>
