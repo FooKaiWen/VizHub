f932b3151b0b636713ddb33576b0710697674162
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2018-11-22 18:13:38 +0800

    Classification

--- classification.py
+++ classification.py
@@ -1,30 +1,31 @@
 import pandas as pd
 oriset = pd.read_csv('10k_5000.csv',encoding='ISO-8859-1')
 dataset = pd.read_csv('dataset.csv',encoding='ISO-8859-1')
-for i in range(oriset.shape[0]):
-    if(pd.notna(oriset.iloc[i,0])):
-        num = oriset.iloc[i,0]
-        # print("start")
-        # print(num)
-        label = num
-        # label = int(label)
-        if(label < 2500):
-            label = 2500
-        elif(label < 5000):
-            label = 5000
-        elif(label < 7500):
-            label = 7500  
-        # elif(label < 8000):
-        #     label = 8000  
-        elif(label < 10000):
-            label = 10000
-        else:
-            label = 9999           
-        dataset.iloc[i,1] = label
+# for i in range(oriset.shape[0]):
+#     if(pd.notna(oriset.iloc[i,0])):
+#         num = oriset.iloc[i,0]
+#         # print("start")
+#         # print(num)
+#         label = num
+#         # label = int(label)
+#         if(label < 2500):
+#             label = 2500
+#         elif(label < 5000):
+#             label = 5000
+#         elif(label < 7500):
+#             label = 7500  
+#         # elif(label < 8000):
+#         #     label = 8000  
+#         elif(label < 10000):
+#             label = 10000
+#         else:
+#             label = 9999           
+#         dataset.iloc[i,1] = label
 
-dataset.to_csv('dataset.csv', encoding='utf-8', index=False)
+# dataset.to_csv('dataset.csv', encoding='utf-8', index=False)
 
 from sklearn import model_selection, preprocessing, linear_model, naive_bayes, metrics, svm
+from sklearn.model_selection import cross_val_score
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn import decomposition, ensemble
 import pandas, numpy, textblob, string
@@ -37,13 +38,15 @@ def train_model(classifier, feature_vector_train, label, feature_vector_valid):
     classifier.fit(feature_vector_train, label)
     # predict the labels on validation dataset
     predictions = classifier.predict(feature_vector_valid)
-    return predictions
-    # return metrics.accuracy_score(predictions, valid_y)
+    # return predictions
+    return metrics.accuracy_score(predictions, valid_y)
 
 dataset = pandas.read_csv('dataset.csv',encoding='ISO-8859-1')
 oriset = pandas.read_csv('5000.csv',encoding='ISO-8859-1')
 
-train_x, valid_x, train_y, valid_y = model_selection.train_test_split(dataset['message'], dataset['two_thousand_likes'])
+labels = dataset['five_thousand_likes']
+
+train_x, valid_x, train_y, valid_y = model_selection.train_test_split(dataset['message'], labels)
 
 count_vect = CountVectorizer(analyzer='word', token_pattern=r'\w{1,}')
 count_vect.fit(dataset['message'])
@@ -52,91 +55,94 @@ count_vect.fit(dataset['message'])
 xtrain_count =  count_vect.transform(train_x)
 xvalid_count =  count_vect.transform(valid_x)
 
-# import numpy as np
-# test = ['new chief staff']
-# test_count = count_vect.transform(test)
+import numpy as np
+test = ['new chief staff']
+test_count = count_vect.transform(test)
 
-# # Naive Bayes on Count Vectors
-# accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, test_count)
-# print("NB, Count Vectors: ", accuracy)
+# Naive Bayes on Count Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_count, train_y, xvalid_count)
+print("NB, Count Vectors: ", accuracy)
 
 
-# # Linear Classifier on Count Vectors
-# accuracy = train_model(linear_model.LogisticRegression(), xtrain_count, train_y, test_count)
-# print("LR, Count Vectors: ", accuracy)
+# Linear Classifier on Count Vectors
+accuracy = train_model(linear_model.LogisticRegression(), xtrain_count, train_y, xvalid_count)
+print("LR, Count Vectors: ", accuracy)
 
-# # RF on Count Vectors
-# accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_count, train_y, test_count)
-# print("RF, Count Vectors: ", accuracy)
+# RF on Count Vectors
+accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_count, train_y, xvalid_count)
+print("RF, Count Vectors: ", accuracy)
 
-# # SVM on Count Vectors
-# accuracy = train_model(svm.SVC(), xtrain_count, train_y, test_count)
-# print("SVM, Count Vectors: ", accuracy)
+# SVM on Count Vectors
+accuracy = train_model(svm.SVC(), xtrain_count, train_y, xvalid_count)
+print("SVM, Count Vectors: ", accuracy)
 
-# # word level tf-idf
-# tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=5000)
-# tfidf_vect.fit(dataset['message'])
-# xtrain_tfidf =  tfidf_vect.transform(train_x)
-# xvalid_tfidf =  tfidf_vect.transform(test)
+# word level tf-idf
+tfidf_vect = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', max_features=1000)
+tfidf_vect.fit(dataset['message'])
 
-# # Naive Bayes on Word Level TF IDF Vectors
-# accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xvalid_tfidf)
-# print("NB, WordLevel TF-IDF: ", accuracy)
+xtrain_tfidf =  tfidf_vect.transform(train_x)
+xvalid_tfidf =  tfidf_vect.transform(valid_x)
 
-# # Linear Classifier on Word Level TF IDF Vectors
-# accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf, train_y, xvalid_tfidf)
-# print("LR, WordLevel TF-IDF: ", accuracy)
+# Naive Bayes on Word Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf, train_y, xvalid_tfidf)
+print("NB, WordLevel TF-IDF: ", accuracy)
 
-# # RF on Word Level TF IDF Vectors
-# accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf, train_y, xvalid_tfidf)
-# print("RF, WordLevel TF-IDF: ", accuracy)
+# Linear Classifier on Word Level TF IDF Vectors
+accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf, train_y, xvalid_tfidf)
+print("LR, WordLevel TF-IDF: ", accuracy)
 
-# # SVM on Word Level TF IDF Vectors
-# accuracy = train_model(svm.SVC(), xtrain_tfidf, train_y, xvalid_tfidf)
-# print("SVM, WordLevel TF-IDF: ", accuracy)
+# RF on Word Level TF IDF Vectors
+accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf, train_y, xvalid_tfidf)
+print("RF, WordLevel TF-IDF: ", accuracy)
 
-# # ngram level tf-idf 
-# tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
-# tfidf_vect_ngram.fit(dataset['message'])
-# xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
-# xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
+# SVM on Word Level TF IDF Vectors
+accuracy = train_model(svm.SVC(), xtrain_tfidf, train_y, xvalid_tfidf)
+print("SVM, WordLevel TF-IDF: ", accuracy)
 
-# # Naive Bayes on Ngram Level TF IDF Vectors
-# accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
-# print("NB, N-Gram Vectors: ", accuracy)
+# ngram level tf-idf 
+tfidf_vect_ngram = TfidfVectorizer(analyzer='word', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=1000)
+tfidf_vect_ngram.fit(dataset['message'])
 
-# # Linear Classifier on Ngram Level TF IDF Vectors
-# accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
-# print("LR, N-Gram Vectors: ", accuracy)
+xtrain_tfidf_ngram =  tfidf_vect_ngram.transform(train_x)
+xvalid_tfidf_ngram =  tfidf_vect_ngram.transform(valid_x)
 
-# # RF on Ngram Level TF IDF Vectors
-# accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
-# print("RF, N-Gram Vectors: ", accuracy)
+# Naive Bayes on Ngram Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("NB, N-Gram Vectors: ", accuracy)
 
-# # SVM on Ngram Level TF IDF Vectors
-# accuracy = train_model(svm.SVC(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
-# print("SVM, N-Gram Vectors: ", accuracy)
+# Linear Classifier on Ngram Level TF IDF Vectors
+accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("LR, N-Gram Vectors: ", accuracy)
+
+# RF on Ngram Level TF IDF Vectors
+accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("RF, N-Gram Vectors: ", accuracy)
+
+# SVM on Ngram Level TF IDF Vectors
+accuracy = train_model(svm.SVC(), xtrain_tfidf_ngram, train_y, xvalid_tfidf_ngram)
+print("SVM, N-Gram Vectors: ", accuracy)
 
 # characters level tf-idf
-tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=5000)
+tfidf_vect_ngram_chars = TfidfVectorizer(analyzer='char', token_pattern=r'\w{1,}', ngram_range=(2,3), max_features=1000)
 tfidf_vect_ngram_chars.fit(dataset['message'])
+
 xtrain_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(train_x) 
 xvalid_tfidf_ngram_chars =  tfidf_vect_ngram_chars.transform(valid_x) 
 
-# # Naive Bayes on Character Level TF IDF Vectors
-# accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
-# print("NB, CharLevel Vectors: ", accuracy)
+# Naive Bayes on Character Level TF IDF Vectors
+accuracy = train_model(naive_bayes.MultinomialNB(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
+print("NB, CharLevel Vectors: ", accuracy)
 
 # Linear Classifier on Character Level TF IDF Vectors
 accuracy = train_model(linear_model.LogisticRegression(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
 print("LR, CharLevel Vectors: ", accuracy)
-predictset = pd.read_csv('comparison2000.csv',encoding='ISO-8859-1')
-i = 0
-for x in np.nditer(accuracy):
-        predictset.iloc[i,1] = x
-        i = i + 1 
+# predictset = pd.read_csv('comparison2000.csv',encoding='ISO-8859-1')
+# i = 0
+# for x in np.nditer(accuracy):
+#         predictset.iloc[i,1] = x
+#         i = i + 1 
 
-predictset.to_csv('comparison2000.csv', encoding='utf-8', index=False)
+# predictset.to_csv('comparison2000.csv', encoding='utf-8', index=False)
 
 # RF on Character Level TF IDF Vectors
 accuracy = train_model(ensemble.RandomForestClassifier(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
@@ -145,3 +151,44 @@ print("RF, CharLevel Vectors: ", accuracy)
 # SVM on Character Level TF IDF Vectors
 accuracy = train_model(svm.SVC(), xtrain_tfidf_ngram_chars, train_y, xvalid_tfidf_ngram_chars)
 print("SVM, CharLevel Vectors: ", accuracy)
+
+total_count_vect = count_vect.transform(dataset['message'])
+total_tfidf_vect = tfidf_vect.transform(dataset['message'])
+total_ngram = tfidf_vect_ngram.transform(dataset['message'])
+total_chars = tfidf_vect_ngram_chars.transform(dataset['message'])
+
+scores = cross_val_score(naive_bayes.MultinomialNB(), total_count_vect, labels, cv=10)
+print("NB CV Count Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(linear_model.LogisticRegression(), total_count_vect, labels, cv=10)
+print("LR CV Count Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(ensemble.RandomForestClassifier(), total_count_vect, labels, cv=10)
+print("RF CV Count Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(svm.SVC(), total_count_vect, labels, cv=10)
+print("SVC CV Count Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+scores = cross_val_score(naive_bayes.MultinomialNB(), total_tfidf_vect, labels, cv=10)
+print("NB tfidf Word Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(linear_model.LogisticRegression(), total_tfidf_vect, labels, cv=10)
+print("LR tfidf Word Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(ensemble.RandomForestClassifier(), total_tfidf_vect, labels, cv=10)
+print("RF tfidf Word Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(svm.SVC(), total_tfidf_vect, labels, cv=10)
+print("SVC tfidf Word Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+scores = cross_val_score(naive_bayes.MultinomialNB(), total_ngram, labels, cv=10)
+print("NB Ngram Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(linear_model.LogisticRegression(), total_ngram, labels, cv=10)
+print("LR Ngram Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(ensemble.RandomForestClassifier(), total_ngram, labels, cv=10)
+print("RF Ngram Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(svm.SVC(), total_ngram, labels, cv=10)
+print("SVC Ngram Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+
+scores = cross_val_score(naive_bayes.MultinomialNB(), total_chars, labels, cv=10)
+print("NB Char Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(linear_model.LogisticRegression(), total_chars, labels, cv=10)
+print("LR Char Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(ensemble.RandomForestClassifier(), total_chars, labels, cv=10)
+print("RF Char Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = cross_val_score(svm.SVC(), total_chars, labels, cv=10)
+print("SVC Char Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
