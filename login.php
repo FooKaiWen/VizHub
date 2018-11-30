3adf5fb239cfdf7ef8c530e6f826070f4541721e
Rick <x3alpine1207@gmail.com>
2018-12-01 00:54:27 +0800

    change redirect url

--- login.php
+++ login.php
@@ -21,15 +21,14 @@
 require_once  'Facebook/autoload.php';
 
 $fb = new Facebook\Facebook([
-  'app_id' => '590344301384464', // Replace {app-id} with your app id
-  'app_secret' => '0e62c56caa857d32e479b8703a7501c4',
+  'app_id' => '267157010556839', // Replace {app-id} with your app id
+  'app_secret' => 'cb8559fb855dcb5a73a624df4fdf58f5',
   'default_graph_version' => 'v3.1',
-
   ]);
 
 $helper = $fb->getRedirectLoginHelper();
 
-$permissions = ['email']; // Optional permissions
+$permissions = ['user_posts','user_tagged_places']; // Optional permissions
 $loginUrl = $helper->getLoginUrl('https://localhost/VizHub/fb-callback.php', $permissions);
 
   if (isset($_POST['logInBtn']))
