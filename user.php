85399df841c556dc85253c8929ec52e4b940c200
chestnutbunker <kelvin_foo_13vin@hotmail.com>
2018-12-03 14:47:32 +0800

    Changes.

--- user.php
+++ user.php
@@ -13,14 +13,14 @@
       <script src="http://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.6/p5.js"></script>  
       <script src ="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>  
 
-    <!-- Bootstrap core CSS-->
-    <link href="vendor/bootstrap/css2/bootstrap.min.css" rel="stylesheet">
+      <!-- Bootstrap core CSS-->
+      <link href="vendor/bootstrap/css2/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+      <!-- Custom fonts for this template-->
+      <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin.css" rel="stylesheet">
+      <!-- Custom styles for this template-->
+      <link href="css/sb-admin.css" rel="stylesheet">
 
       <?php
          require_once  'Facebook/autoload.php';
@@ -34,47 +34,41 @@
          $query = new MongoDB\Driver\Query([]);
          
          
-         $userdatas = $connection->executeQuery('fb.userDetail', $query);
+         $userDatas = $connection->executeQuery('fb.userDetail', $query);
          
-         foreach($userdatas as $userdata){
-         $name = $userdata->name;
-         $id = $userdata->id;
-         $url =$userdata->url;
+         foreach($userDatas as $userData){
+            $name = $userData->name;
+            $id = $userData->id;
+            $url =$userData->url;
          }
          
          $newdb = $client->selectDatabase('fb');
          $temp = $newdb->selectCollection('predictMessage');
          $temp->drop();
-         $messagecol = $newdb->selectCollection('predictMessage');
+         $messageCol = $newdb->selectCollection('predictMessage');
          
          session_start();
          $logoutUrl = $_SESSION['logoutUrl'];
-         
-         
          ?>
    </head>
    <body>
       <nav class="navbar navbar-expand navbar-dark bg-dark static-top">  
          <a class="navbar-brand mr-1" href="user.php">VizHub</a>  
          <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">  
-         <i class="fas fa-bars"></i>  
+            <i class="fas fa-bars"></i>  
          </button>  
          <a class="navbar-brand" href="user.php">Home</a>  
          <a class="navbar-brand" href="http://localhost/VizHub/aboutUs.html">About Us</a>  
-
-                   <div class="collapse navbar-collapse" id="navbarResponsive">
-              <ul class="navbar-nav ml-auto">
-
-                  <li>
+         <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+               <li>
                   <button style = "float:right;"class ="Btn Btn--facebook" onclick="logout()">Log Out</button>
-                  </li>
-              </ul>
-          </div>
-
-         
+               </li>
+            </ul>
+         </div>
       </nav>
 
-       <div id="wrapper">
+   <div id="wrapper">
       <!-- Sidebar -->
       <ul class="sidebar navbar-nav">
          <li class="nav-item active">
@@ -115,32 +109,31 @@
                </div> 
             </div>
 
-
             <?php
-               if(isset($_REQUEST['submit_btn'])){
-               
+               if(isset($_REQUEST['submitBtn'])){
                  $message = $_POST["predictM"];
+                 
                  if($message != ""){
-                   $messagecol->insertOne(
+                   $messageCol->insertOne(
                      [
                      '_id'=>'message',
                      'pmessage'=>"$message"]);
                    shell_exec("python readtext.py");
-                   $upperboundary = $messagecol->findOne()->likesRange;
-                   if($upperboundary == 50){
-                     $lowerboundary = 0;
+                   $upperBoundary = $messageCol->findOne()->likesRange;
+                   if($upperBoundary == 50){
+                     $lowerBoundary = 0;
                    } else {
-                     $lowerboundary = $upperboundary - 49;
+                     $lowerBoundary = $upperBoundary - 49;
                    }
                
                    echo '
                    <label for="summary" style="margin-top:15px; margin-left:30px;"><i>Result</i></label>
                    <div id="summary" style="font-size:20px;">The machine predicted that the number of likes is somewhat around <strong style="background-color:White; color:Black;">';
                  
-                   if($upperboundary == 250){
-                     echo htmlspecialchars($upperboundary).' and above</strong>';
+                   if($upperBoundary == 250){
+                     echo htmlspecialchars($upperBoundary).' and above</strong>';
                    } else {
-                     echo htmlspecialchars($lowerboundary).' to '.htmlspecialchars($upperboundary).'</strong>';
+                     echo htmlspecialchars($lowerBoundary).' to '.htmlspecialchars($upperBoundary).'</strong>';
                    }
                    
                    echo ' for the message: '.htmlspecialchars($message).'</div>';   
@@ -152,8 +145,7 @@
                }
                ?>
             </div>
-
-                        <div class="card mb-3">
+            <div class="card mb-3">
                <div class="card-header">
                   <i class="fas fa-chart-area"></i>
                   Prediction
@@ -164,40 +156,34 @@
                         <div class="form-group" >
                            <label for="Message" style="margin-top :15px;"><i>Message for Like Prediction:</i></label>
                            <textarea class="form-control" style ="border: 3px solid rgb(47, 52, 78); " name="predictM" rows="3" id="message" 
-                              placeholder="Type Your Message Here For Like Prediction . . . . . ."><?php if(isset($_REQUEST['submit_btn'])){echo htmlspecialchars($message);}?></textarea>
+                              placeholder="Type Your Message Here For Like Prediction . . . . . ."><?php if(isset($_REQUEST['submitBtn'])){echo htmlspecialchars($message);}?></textarea>
                            <div style ="text-align:center;">  
                               <button class ="copyText" onclick="copyMessage();return false;">Copy<br> Message</button>
-                              <button class ="predict" type="submit" name="submit_btn">Predict <br> likes</button>
+                              <button class ="predict" type="submit" name="submitBtn">Predict <br> likes</button>
                            </div>
                         </div>
                      </div>
                   </form>
                </div>
             </div>
-     
          </div>
          <!-- /.container-fluid -->
       </div>
+   </div>
 
-      </div>
+      <script>
+         function copyMessage() {
+            var copyText = document.getElementById("message");
+            copyText.select();
+            document.execCommand("copy");
+            alert("Copied the text: " + copyText.value);
+         }
 
-       <script>
-      function copyMessage() {
-        var copyText = document.getElementById("message");
-        copyText.select();
-        document.execCommand("copy");
-        alert("Copied the text: " + copyText.value);
-      }
-
-      function logout(){
-        var logoutUrl = <?php echo json_encode($logoutUrl);?>;
-
-        window.location = logoutUrl;
-      }
-      
-</script>
- 
-
+         function logout(){
+            var logoutUrl = <?php echo json_encode($logoutUrl);?>;
+            window.location = logoutUrl;
+         }
+      </script>
       <!-- Bootstrap core JavaScript-->
       <script src="vendor/jquery/jquery.min.js"></script>
       <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
